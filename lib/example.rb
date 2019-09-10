e413436f5a9c5066c98c664777e246c27260a443
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-10 02:15:53 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -2,12 +2,12 @@
 def say_hello_world_ten_times
   phrase = "Hello World!"
   puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
 puts phrase
 puts phrase
 puts phrase
